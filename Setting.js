8483ce2928feac093870eff09f12d96486d05413
Sierra Neumann <neu21001@byui.edu>
2022-02-17 16:15:55 -0700

    commit on 'main'

--- Setting.js
+++ Setting.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert,   } from 'react-native';
+import UselessTextInput from "./UselessTextInput";
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -9,10 +10,45 @@ function Setting() {
       <View>
       <Button
       title="Log In"
-      onPress={() => Alert.alert('Simple Button pressed')}
+      onPress={() => Alert.alert('Enter Phone Number')}
     />
       </View>
+
+      
     )
   }
-
-export default Setting;
+  import React from "react";
+  import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+  
+  const UselessTextInput = () => {
+    const [text, onChangeText] = React.useState("Useless Text");
+    const [number, onChangeNumber] = React.useState(null);
+  
+    return (
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+  
+  export default Setting;
